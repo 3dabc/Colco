f9d2ad425f7084abe9d0776af99cceac80ad37b3
Jose Lee <andrelee089@gmail.com>
2025-05-05 10:41:33 -0700

    remove chirpstack

--- python-backend/app.py
+++ python-backend/app.py
@@ -3,9 +3,19 @@ from flask_cors import CORS
 from endpoints.auth import auth_bp
 from endpoints.sensor_data import sensor_data_bp
 from endpoints.gps_data import geo_data_bp
-from endpoints.chirpstack import chirpstack_bp
 
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv
+with open('secret-key.json') as f:
+    secret_key= json.load(f)['SECRET_KEY']
+    
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = secret_key
+
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 CORS(app)
 
@@ -13,7 +23,5 @@ CORS(app)
 app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
 app.register_blueprint(sensor_data_bp, url_prefix='/api/v1/sensor')
 app.register_blueprint(geo_data_bp, url_prefix='/api/v1/geo')
-app.register_blueprint(chirpstack_bp, url_prefix='/api/v1/chirpstack')
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
